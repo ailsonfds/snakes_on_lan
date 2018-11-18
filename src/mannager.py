1eae9ee08c32c8080daa75f8a7c92223039c680e
Ailson F. dos Santos <ailsonfds94@gmail.com>
2018-11-17 22:37:35 -0300

    Corrrigido bug da posição inicial.

--- src/mannager.py
+++ src/mannager.py
@@ -10,7 +10,12 @@ from time import sleep
 from random import choice
 
 class Mannager(object):
-    """docstring for Mannager"""
+    """
+    docstring for Mannager
+
+    This class is responsible for mannager each match.
+    The purpose is to make this the server side of the main application.
+    """
     players={}
     players_colors={}
 
@@ -34,17 +39,25 @@ class Mannager(object):
         return score_dict
 
 def play(arena):
-    p1_color='black'
-    p1_name='player1'
-    p1_snk=Snake(p1_color,[14,14])
-    p1_snk.next_mov=choice([[-1,0],[1,0],[0,1],[0,-1]])
     Snake.xlims=29
     Snake.ylims=29
+    p1_color='black'
+    p1_name='player1'
+    p1_snk=Snake(color=p1_color,initial_pos=[14,14])
+    p1_snk.next_mov=choice([[-1,0],[1,0],[0,1]])
+    p2_color='red'
+    p2_name='player2'
+    p2_snk=Snake(color=p2_color,initial_pos=[24,24])
+    p2_snk.next_mov=choice([[-1,0],[1,0],[0,1]])
     arena.master.bind('<Up>', p1_snk.up)
     arena.master.bind('<Down>', p1_snk.down)
     arena.master.bind('<Left>', p1_snk.left)
     arena.master.bind('<Right>', p1_snk.right)
-    arena.update_score({p1_name:p1_snk.size})
+    arena.master.bind('<w>', p2_snk.up)
+    arena.master.bind('<s>', p2_snk.down)
+    arena.master.bind('<a>', p2_snk.left)
+    arena.master.bind('<d>', p2_snk.right)
+    arena.create_score({p1_name:p1_snk.size, p2_name:p2_snk.size})
     for tile in p1_snk.loc:
         arena.draw_tile(tile[0],tile[1],p1_color)
     while arena.master.state()=='normal':
@@ -54,9 +67,18 @@ def play(arena):
             arena.draw_tile(pos[0],pos[1],'white')
             arena.update()
         if 'crash' in msg:
-            arena.lose_msg('P1 HAVE LOST')
+            arena.lose_msg('P1 HAVE\nSELF\nCRASHED')
             break
-        sleep(1)
+        msg, pos=p2_snk.move(p2_snk.next_mov[0],p2_snk.next_mov[1])
+        if msg=='walk':
+            arena.draw_tile(p2_snk.head[0],p2_snk.head[1],p2_color)
+            arena.draw_tile(pos[0],pos[1],'white')
+            arena.update()
+        if 'crash' in msg:
+            arena.lose_msg('P2 HAVE\nSELF\nCRASHED')
+            break
+        arena.update_score({p1_name:p1_snk.size, p2_name:p2_snk.size})
+        sleep(0.1)
 
 def main():
     root=Tk()
